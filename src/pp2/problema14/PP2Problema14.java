dd74f3283d2350b29b0540279a7b7acf3053127e
PC <PC@192.168.1.70>
2016-10-15 17:47:36 -0500

    14.	Crear una tabla de tamaño 7x7 y rellenarla de forma que los elementos de la diagonal principal sean 1 y el resto 0.

--- src/pp2/problema14/PP2Problema14.java
+++ src/pp2/problema14/PP2Problema14.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package pp2.problema14;
-
 /**
  *
  * @author PC
@@ -15,7 +14,23 @@ public class PP2Problema14 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[][]tabla= new int [7][7];
+        tabla=llenarTabla(tabla);
+        mostrarTabla(tabla);
     }
-    
+    public static int[][] llenarTabla(int[][]tabla){
+        for (int i=0; i<10; i++){ //aumenta i una vez
+            for (int j=0; j<10; j++){ //aumenta j una vez (hasta llegar a 10)
+                tabla[i][j]=i*j;
+            }
+        }
+       return tabla;
+    }
+    public static void mostrarTabla(int [][]b){
+        for(int i=0; i<10; i++){
+         for(int j=0; j<10; j++){ 
+              System.out.print(i + "x" + j + "=" + b[i][j] + "\n"); 
+         }
+       }      
+    }
 }
